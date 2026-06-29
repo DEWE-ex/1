3f3c030ed0e749beef4a4eae3e64aa9a3fc8414d
DEWE-ex <doeverythingew@gmail.com>
2026-06-29 09:45:27 +0700

    Delete 1 directory

--- 1/src/app/(app)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import ShellGate from "@/components/shell/ShellGate";
-
-export default function AppLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <ShellGate>{children}</ShellGate>;
-}
